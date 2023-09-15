60b8674b2a63b4aade9b359d53265a95b4b65541
Asif Syeed <asifsyeed666@gmail.com>
2023-09-16 03:31:51 +0600

    Header and footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import FooterComponents from './components/FooterComponents';
+import HeaderComponent from './components/HeaderComponent';
 import ListOfEventsComponent from './components/ListOfEventsComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent/>
       <ListOfEventsComponent/>
+      <FooterComponents/>
     </div>
   );
 }
---
+++ src/components/FooterComponents.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponents = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span className="text-muted">
+                All rights reserved 2023
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponents
